03f78623f6b3d8091a444cf07a2567bc909d3c28
hz <hz@172.19.115.3>
2017-08-20 11:10:33 +0800

    优化上传

--- kindArt-background/src/main/java/com/kindArt/manager/controller/FileUploaderController.java
+++ kindArt-background/src/main/java/com/kindArt/manager/controller/FileUploaderController.java
@@ -1,5 +1,6 @@
 package com.kindArt.manager.controller;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -31,29 +32,49 @@ public class FileUploaderController {
 		// 当前用户
 		InputStream inputStream = null;
 		FileOutputStream fileOutputStream = null;
-		String filePath = "D:/";
+		String filePath = "D:/testfilePath";
+
 		try {
+			File file = new File(filePath);
+			if(!file.exists()){
+				file.mkdirs();
+			}
 			// 判断 request 是否有文件上传,即多部分请求
 			if (multipartResolver.isMultipart(request)) {
 				// 转换成多部分request
 				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
 				// 取得request中的所有文件名
 				Iterator<String> iter = multiRequest.getFileNames();
+				String fileFullPath = filePath;
 				while (iter.hasNext()) {
 					MultipartFile multipartFile = multiRequest.getFile(iter
 							.next());
-					String fileName = multipartFile.getOriginalFilename()
-							.substring(
-									0,
-									multipartFile.getOriginalFilename()
-											.lastIndexOf("."));
-//					String suffix = multipartFile.getOriginalFilename()
-//							.substring(
-//									multipartFile.getOriginalFilename()
-//											.lastIndexOf("."));
+					String fileName = multipartFile.getOriginalFilename();
 					// 写入文件到临时目录
 					inputStream = multipartFile.getInputStream();
-					fileOutputStream = new FileOutputStream(filePath+fileName);
+					fileOutputStream = new FileOutputStream(filePath + fileName);
+					byte buffer[] = new byte[4096]; // create a buffer
+					long fileSize = multipartFile.getSize();
+					if (fileSize <= buffer.length) {// if fileSize < buffer
+						buffer = new byte[(int) fileSize];
+					}
+					int line = 0;
+					while ((line = inputStream.read(buffer)) > 0) {
+						fileOutputStream.write(buffer, 0, line);
+					}
+				}
+			} else {
+				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+				// 取得request中的所有文件名
+				Iterator<String> iter = multiRequest.getFileNames();
+				String fileFullPath = filePath;
+				if (iter.hasNext()) {
+					MultipartFile multipartFile = multiRequest.getFile(iter
+							.next());
+					String fileName = multipartFile.getOriginalFilename();
+					// 写入文件到临时目录
+					inputStream = multipartFile.getInputStream();
+					fileOutputStream = new FileOutputStream(filePath + fileName);
 					byte buffer[] = new byte[4096]; // create a buffer
 					long fileSize = multipartFile.getSize();
 					if (fileSize <= buffer.length) {// if fileSize < buffer
@@ -66,7 +87,7 @@ public class FileUploaderController {
 				}
 			}
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		} finally {
 			if (fileOutputStream != null) {
 				fileOutputStream.close();
