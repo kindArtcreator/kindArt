74eb6d286ed9432d01a4d2ea4594186c4e1c2ac5
hz <hz@hz-office>
2017-08-20 23:33:54 +0800

    优化upload

--- kindArt-background/src/main/java/com/kindArt/manager/controller/FileUploaderController.java
+++ kindArt-background/src/main/java/com/kindArt/manager/controller/FileUploaderController.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +29,7 @@ public class FileUploaderController {
 
 		String returnJson = "";
 
+		
 		// 创建一个通用的多部分解析器
 		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
 				request.getSession().getServletContext());
@@ -34,6 +38,7 @@ public class FileUploaderController {
 		FileOutputStream fileOutputStream = null;
 		String filePath = "D:/testfilePath";
 
+		
 		try {
 			File file = new File(filePath);
 			if (!file.exists()) {
@@ -43,12 +48,17 @@ public class FileUploaderController {
 			if (multipartResolver.isMultipart(request)) {
 				// 转换成多部分request
 				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+				
 				// 取得request中的所有文件名
-				Iterator<String> iter = multiRequest.getFileNames();
-				String fileFullPath = filePath;
-				while (iter.hasNext()) {
-					MultipartFile multipartFile = multiRequest.getFile(iter
-							.next());
+				Map<String, MultipartFile> map = multiRequest.getFileMap();
+				
+				MultiValueMap<String, MultipartFile> mvmp= multiRequest.getMultiFileMap();
+				
+				List<MultipartFile> list = mvmp.get("HTML_Browse_up");
+				
+				for (Iterator<MultipartFile> iterator = list.iterator(); iterator.hasNext();) {
+					MultipartFile multipartFile = iterator
+							.next();
 					String fileName = multipartFile.getOriginalFilename();
 					// 写入文件到临时目录
 					inputStream = multipartFile.getInputStream();
@@ -62,28 +72,9 @@ public class FileUploaderController {
 					while ((line = inputStream.read(buffer)) > 0) {
 						fileOutputStream.write(buffer, 0, line);
 					}
-				}
-			} else {
-				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-				// 取得request中的所有文件名
-				Iterator<String> iter = multiRequest.getFileNames();
-				String fileFullPath = filePath;
-				if (iter.hasNext()) {
-					MultipartFile multipartFile = multiRequest.getFile(iter
-							.next());
-					String fileName = multipartFile.getOriginalFilename();
-					// 写入文件到临时目录
-					inputStream = multipartFile.getInputStream();
-					fileOutputStream = new FileOutputStream(filePath + fileName);
-					byte buffer[] = new byte[4096]; // create a buffer
-					long fileSize = multipartFile.getSize();
-					if (fileSize <= buffer.length) {// if fileSize < buffer
-						buffer = new byte[(int) fileSize];
-					}
-					int line = 0;
-					while ((line = inputStream.read(buffer)) > 0) {
-						fileOutputStream.write(buffer, 0, line);
-					}
+					fileOutputStream.flush();
+					fileOutputStream.close();
+					inputStream.close();
 				}
 			}
 		} catch (Exception e) {
@@ -99,36 +90,4 @@ public class FileUploaderController {
 		}
 	}
 
-	@RequestMapping(value = "uploaderOrg", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
-	@ResponseBody
-	public String getDataFromFile(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-
-		String returnJson = "";
-		
-		
-
-		// 当前用户
-		InputStream inputStream = null;
-		FileOutputStream fileOutputStream = null;
-		String filePath = "D:/testfilePath";
-
-		try {
-			File file = new File(filePath);
-			if (!file.exists()) {
-				file.mkdirs();
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (fileOutputStream != null) {
-				fileOutputStream.close();
-			}
-			if (inputStream != null) {
-				inputStream.close();
-			}
-			return returnJson;
-		}
-	}
 }
